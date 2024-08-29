0a65afad0593efbae6ffef7a5e49c84123802058
jefin38 <147368514+jefin38@users.noreply.github.com>
2024-08-29 13:28:16 +0530

    Update 9 area of circle.py

--- 9 area of circle.py
+++ 9 area of circle.py
@@ -2,3 +2,31 @@ pi = 3.14
 r=float(input("enter radius"))
 area = pi*r**2
 print(area)
+
+from numpy import array
+
+from scipy.linalg import svd
+
+#define a matrix
+
+A = array([[1, 2], [3, 4], [5, 6]])
+
+print("A = ",A)
+
+print("Shape of array A: ",A.shape)
+
+print("")
+
+U, pp. VT svd(A)# SVD
+
+print("U = ",U)
+
+print("Shape of matrix U: ", U.shape)
+
+print("")
+
+print("Sigma(diagonal matrix), s = ",s)
+
+print("Shape of matrix sigma: ",s.shape)
+
+print("")
